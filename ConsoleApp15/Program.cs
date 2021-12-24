08605e0c39d4d3d651d2018fcfb090a90dcef121
User <User@DESKTOP-C7DGT0E>
2021-12-24 13:02:30 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tashoglo15High
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число 1234:");
+            int a = int.Parse(Console.ReadLine());
+            Reverse(a);
+            Console.ReadKey();
+        }
+
+        static void Reverse(int chislo)
+        {
+            int revchislo = chislo % 10;
+            Console.Write(revchislo);
+            chislo /= 10;
+
+            if (chislo > 0)
+                Reverse(chislo);
+        }
+    }
+}
